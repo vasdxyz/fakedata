87e751db24eed514aa4bf1caf27848edc09e5db0
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-01-21 13:58:06 +0200

    v0.0.1

---
+++ Runtime/FakeDataBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xyz.Vasd.FakeData
+{
+    [AddComponentMenu("FakeData/" + nameof(FakeDataBase))]
+
+    public class FakeDataBase : MonoBehaviour
+    {
+        private Dictionary<Type, object> _singletons = new();
+
+        public T GetSingleton<T>() where T : class, new()
+        {
+            var type = typeof(T);
+            if (_singletons.ContainsKey(type)) return (T)_singletons[type];
+
+            var singleton = new T();
+            _singletons[type] = singleton;
+
+            return singleton;
+        }
+    }
+}
